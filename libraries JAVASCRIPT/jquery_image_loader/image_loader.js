a46920142df2baac285fd1acc7c7efba3ad20326
Owen <owen@skylogix.net>
2014-06-11 18:16:49 -0400

    thumbnails added
    Signed-off-by: Owen <owen@skylogix.net>

--- libraries JAVASCRIPT/jquery_image_loader/image_loader.js
+++ libraries JAVASCRIPT/jquery_image_loader/image_loader.js
@@ -2,20 +2,20 @@
  * Author: Owen Peredo Diaz
  * This is a jquery plugin that add two new methods to load image (start, progress)
  * With these new functions you can capture the progress status and show its progress bar
- * PARAMS: complete: function(img){}, progress: function(e, img){}, start: function(img){}
+ * PARAMS: complete: function(e, img){}, progress: function(e, img){}, start: function(e, img){}, error: function(e, img){}
  * samples:
  <script>
  function load_img(url)
  {
      var image_to_load =$("<img src='"+url+"'>");
      var loading = $(".loading");
-     image_to_load.load_img({start: function(img)
+     image_to_load.load_img({start: function(e, img)
          {
              loading.show().html("0%");
          }, progress: function(e, img)
          {
              loading.html(Math.round(e.loaded / e.total * 100)+"%");
-         }, complete: function(){
+         }, complete: function(e, img){
              $("body").append(image_to_load);
              loading.html("complete").hide();
          }});
@@ -32,7 +32,7 @@
  */
 jQuery(function($){
     $.fn.load_img = function(params){
-        var settings = $.extend({complete: function(){}, progress: function(){}, start: function(){}}, params);
+        var settings = $.extend({complete: function(e, img){}, progress: function(e, img){}, start: function(e, img){}, error: function(e, img){}}, params);
         $(this).each(function(){
             var img = $(this);
             if(typeof XMLHttpRequest === "undefined"){
@@ -43,10 +43,10 @@ jQuery(function($){
                 request = new XMLHttpRequest();
                 request.onloadstart = function(){ settings.start(img); };
                 request.onprogress = function(e){ settings.progress(e, img); };
-                request.addEventListener('progress',function(e){ settings.progress(e, img); }, false);
-                request.onload = function(){  try{ img.attr("src", "data:image/jpeg;base64," + base64Encode_(request.responseText));  }catch(e){} };
-                request.onloadend = function(){ settings.complete(img); };
+                try{request.addEventListener('progress',function(e){ settings.progress(e, img); }, false);} catch(e){ request.onreadystatechange=function() { if (request.readyState==4 && request.status==200) { settings.complete(img); } } } //ie7, 8 fix
+                request.onload = function(){ try{ img.attr("src", "data:image/jpeg;base64," + base64Encode_(request.responseText));  }catch(e){} settings.complete(img); };
                 request.open("GET", img.attr("src"), true);
+                request.onerror = function(e){ settings.error(e, img); }
                 //request.responseType = 'arraybuffer'; //with this param is not posible to get request.responseText
                 try{ request.overrideMimeType('text/plain; charset=x-user-defined'); }catch(e){}
                 request.send(null);
@@ -59,7 +59,6 @@ jQuery(function($){
             var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
             var outputStr = "";
             var i = 0;
-
             while (i < inputStr.length)
             {
                 //all three "& 0xff" added below are there to fix a known bug
@@ -67,31 +66,12 @@ jQuery(function($){
                 var byte1 = inputStr.charCodeAt(i++) & 0xff;
                 var byte2 = inputStr.charCodeAt(i++) & 0xff;
                 var byte3 = inputStr.charCodeAt(i++) & 0xff;
-
                 var enc1 = byte1 >> 2;
                 var enc2 = ((byte1 & 3) << 4) | (byte2 >> 4);
-
                 var enc3, enc4;
-                if (isNaN(byte2))
-                {
-                    enc3 = enc4 = 64;
-                }
-                else
-                {
-                    enc3 = ((byte2 & 15) << 2) | (byte3 >> 6);
-                    if (isNaN(byte3))
-                    {
-                        enc4 = 64;
-                    }
-                    else
-                    {
-                        enc4 = byte3 & 63;
-                    }
-                }
-
+                if (isNaN(byte2)) { enc3 = enc4 = 64; } else { enc3 = ((byte2 & 15) << 2) | (byte3 >> 6); if (isNaN(byte3)) { enc4 = 64; } else { enc4 = byte3 & 63; } }
                 outputStr += b64.charAt(enc1) + b64.charAt(enc2) + b64.charAt(enc3) + b64.charAt(enc4);
             }
-
             return outputStr;
         }
     }
